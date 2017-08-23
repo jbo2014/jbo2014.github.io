bbc1581c88315a0c337a3b9f961cfd71ff457254
laowang <jbo2014haha@outlook.com>
2017-08-23 12:37:50 +0800

    20170823 Edit

--- js/main.js
+++ js/main.js
@@ -45,88 +45,88 @@ jQuery(function($) {'use strict';
 	});
 
 	//Slider
-	$(document).ready(function() {
-		var time = 7; // time in seconds
+	// $(document).ready(function() {
+	// 	var time = 7; // time in seconds
 
-	 	var $progressBar,
-	      $bar, 
-	      $elem, 
-	      isPause, 
-	      tick,
-	      percentTime;
+	//  	var $progressBar,
+	//       $bar, 
+	//       $elem, 
+	//       isPause, 
+	//       tick,
+	//       percentTime;
 	 
-	    //Init the carousel
-	    $("#main-slider").find('.owl-carousel').owlCarousel({
-	      slideSpeed : 500,
-	      paginationSpeed : 500,
-	      singleItem : true,
-	      navigation : true,
-			navigationText: [
-			"<i class='fa fa-angle-left'></i>",
-			"<i class='fa fa-angle-right'></i>"
-			],
-	      afterInit : progressBar,
-	      afterMove : moved,
-	      startDragging : pauseOnDragging,
-	      //autoHeight : true,
-	      transitionStyle : "fadeUp"
-	    });
+	//     //Init the carousel
+	//     $("#main-slider").find('.owl-carousel').owlCarousel({
+	//       slideSpeed : 500,
+	//       paginationSpeed : 500,
+	//       singleItem : true,
+	//       navigation : true,
+	// 		navigationText: [
+	// 		"<i class='fa fa-angle-left'></i>",
+	// 		"<i class='fa fa-angle-right'></i>"
+	// 		],
+	//       afterInit : progressBar,
+	//       afterMove : moved,
+	//       startDragging : pauseOnDragging,
+	//       //autoHeight : true,
+	//       transitionStyle : "fadeUp"
+	//     });
 	 
-	    //Init progressBar where elem is $("#owl-demo")
-	    function progressBar(elem){
-	      $elem = elem;
-	      //build progress bar elements
-	      buildProgressBar();
-	      //start counting
-	      start();
-	    }
+	//     //Init progressBar where elem is $("#owl-demo")
+	//     function progressBar(elem){
+	//       $elem = elem;
+	//       //build progress bar elements
+	//       buildProgressBar();
+	//       //start counting
+	//       start();
+	//     }
 	 
-	    //create div#progressBar and div#bar then append to $(".owl-carousel")
-	    function buildProgressBar(){
-	      $progressBar = $("<div>",{
-	        id:"progressBar"
-	      });
-	      $bar = $("<div>",{
-	        id:"bar"
-	      });
-	      $progressBar.append($bar).appendTo($elem);
-	    }
+	//     //create div#progressBar and div#bar then append to $(".owl-carousel")
+	//     function buildProgressBar(){
+	//       $progressBar = $("<div>",{
+	//         id:"progressBar"
+	//       });
+	//       $bar = $("<div>",{
+	//         id:"bar"
+	//       });
+	//       $progressBar.append($bar).appendTo($elem);
+	//     }
 	 
-	    function start() {
-	      //reset timer
-	      percentTime = 0;
-	      isPause = false;
-	      //run interval every 0.01 second
-	      tick = setInterval(interval, 10);
-	    };
+	//     function start() {
+	//       //reset timer
+	//       percentTime = 0;
+	//       isPause = false;
+	//       //run interval every 0.01 second
+	//       tick = setInterval(interval, 10);
+	//     };
 	 
-	    function interval() {
-	      if(isPause === false){
-	        percentTime += 1 / time;
-	        $bar.css({
-	           width: percentTime+"%"
-	         });
-	        //if percentTime is equal or greater than 100
-	        if(percentTime >= 100){
-	          //slide to next item 
-	          $elem.trigger('owl.next')
-	        }
-	      }
-	    }
+	//     function interval() {
+	//       if(isPause === false){
+	//         percentTime += 1 / time;
+	//         $bar.css({
+	//            width: percentTime+"%"
+	//          });
+	//         //if percentTime is equal or greater than 100
+	//         if(percentTime >= 100){
+	//           //slide to next item 
+	//           $elem.trigger('owl.next')
+	//         }
+	//       }
+	//     }
 	 
-	    //pause while dragging 
-	    function pauseOnDragging(){
-	      isPause = true;
-	    }
+	//     //pause while dragging 
+	//     function pauseOnDragging(){
+	//       isPause = true;
+	//     }
 	 
-	    //moved callback
-	    function moved(){
-	      //clear interval
-	      clearTimeout(tick);
-	      //start again
-	      start();
-	    }
-	});
+	//     //moved callback
+	//     function moved(){
+	//       //clear interval
+	//       clearTimeout(tick);
+	//       //start again
+	//       start();
+	//     }
+	// });
 
 	//Initiat WOW JS
 	new WOW().init();
